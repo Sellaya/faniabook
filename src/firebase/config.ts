5a13d8aeef8fa49790f8406fe750246ec13a9a1e
Shan Ali <shaneali720@gmail.com>
2025-11-12 21:07:33 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2795078730-f2231",
+  "appId": "1:443181454969:web:7f2457af64018bf73566a8",
+  "apiKey": "AIzaSyDp4NHVqAuQSKw49S86OhjEnM8SrY-ISH0",
+  "authDomain": "studio-2795078730-f2231.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "443181454969"
+};
